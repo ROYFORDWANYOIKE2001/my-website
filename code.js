f76037ce42cc363f24432459f35749f102951f91
RoyfordWanyoike <60444753+ROYFORDWANYOIKE2001@users.noreply.github.com>
2020-09-26 12:46:16 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <meta charset="utf-8", lang = "en">
+      <title>#100DaysOfCode</title>
+</head> 
+<body>
+      <script>
+              var greet = "Hello World"
+              document.write(greet);
+       </script>           
+</body> 
+</html>     
